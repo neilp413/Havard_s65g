fc80df770a3298df31e52d7ee84723973f14faa4
nileriver413 <neil.patel.2000@gmail.com>
2016-06-27 12:25:01 -0400

    Finish Assignment 1

---
+++ Class2/Class2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Class2
+//
+//  Created by Neil Patel on 6/22/16.
+//  Copyright © 2016 Neil Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
